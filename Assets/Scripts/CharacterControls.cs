869f31f4072143f56c2b8380ee077abe57420dc0
Nick Maltbie <nick.dmalt@gmail.com>
2022-04-01 19:01:14 -0700

    Added some basic code formatting and cleanup

--- Assets/Scripts/CharacterControls.cs
+++ Assets/Scripts/CharacterControls.cs
@@ -1,29 +1,63 @@
+﻿// Copyright (C) 2022 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 namespace CCDemo
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.InputSystem;
 
+    /// <summary>
+    /// Basic character Controls for moving a player around in 3D space.
+    /// </summary>
     public class CharacterControls : MonoBehaviour
     {
+        /// <summary>
+        /// Look action to modify the player camera.
+        /// </summary>
         public InputAction lookAction;
 
+        /// <summary>
+        /// Move action to move the player around.
+        /// </summary>
         public InputAction moveAction;
 
+        /// <summary>
+        /// Speed of player movement in units per second.
+        /// </summary>
         public float playerSpeed = 5.0f;
 
+        /// <summary>
+        /// Speed of player rotation in degrees per second.
+        /// </summary>
         public float rotationSpeed = 180.0f;
 
-        // Start is called before the first frame update
-        void Start()
+        /// <summary>
+        /// Setup for character controls.
+        /// </summary>
+        public void Start()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Update function for character controls.
+        /// </summary>
+        public void Update()
         {
 
         }
--- Assets/Tests/CharacterControlsTest.cs
+++ Assets/Tests/CharacterControlsTest.cs
@@ -1,3 +1,20 @@
+﻿// Copyright (C) 2022 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 namespace CCDemo.Tests
 {
@@ -7,6 +24,9 @@ namespace CCDemo.Tests
     using UnityEngine.InputSystem;
     using UnityEngine.TestTools;
 
+    /// <summary>
+    /// Tests for character controls to ensure character controller acts as expected.
+    /// </summary>
     public class CharacterControlsTest : InputTestFixture
     {
         /// <summary>
@@ -19,12 +39,17 @@ namespace CCDemo.Tests
         /// </summary>
         private Gamepad gamepad;
 
-        [UnitySetUp]
-        public IEnumerator UnitySetUp()
+        /// <summary>
+        /// Setup code run before each test.
+        /// </summary>
+        [SetUp]
+        public override void Setup()
         {
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            base.Setup();
+
+            var go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
             this.character = go.AddComponent<CharacterControls>();
-            var playerInput = go.AddComponent<PlayerInput>();
+            _ = go.AddComponent<PlayerInput>();
 
             // setup basic inputs in test
             this.character.moveAction = new InputAction(type: InputActionType.PassThrough, binding: "<Gamepad>/leftStick");
@@ -33,35 +58,36 @@ namespace CCDemo.Tests
             this.character.moveAction.Enable();
             this.character.lookAction.Enable();
 
-            yield return null;
-        }
-
-        [SetUp]
-        public override void Setup()
-        {
-            base.Setup();
-
             // Setup gamepad, needs to be done in SetUp method
             this.gamepad = InputSystem.AddDevice<Gamepad>();
         }
 
+        /// <summary>
+        /// Tear down code used to cleanup tests.
+        /// </summary>
         [TearDown]
         public override void TearDown()
         {
+            GameObject.DestroyImmediate(this.character.gameObject);
             base.TearDown();
-            Object.Destroy(this.character.gameObject);
         }
 
+        /// <summary>
+        /// Validate player movement for a give period of time in a given direction.
+        /// </summary>
+        /// <param name="time">Expected time to pass</param>
+        /// <param name="expected">Expected distance for player to move.</param>
+        /// <returns>Enumerator of waiting events for player movement.</returns>
         public IEnumerator ValidateMovement(float time, Vector3 expected)
         {
             // Measure start position
-            Vector3 start = character.transform.position;
+            Vector3 start = this.character.transform.position;
 
             // Wait for a second
             yield return new WaitForSeconds(time);
-            
+
             // Assert that the player has moved forward
-            Vector3 end = character.transform.position;
+            Vector3 end = this.character.transform.position;
             Vector3 movement = end - start;
             float delta = (expected - movement).magnitude;
 
@@ -75,54 +101,54 @@ namespace CCDemo.Tests
         /// <summary>
         /// Move forward when the player presses forward
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Enumerator test events.</returns>
         [UnityTest]
         public IEnumerator CharacterControlsMoveForward()
         {
             // Set input action to move forward
             float time = 1.0f;
-            Set<Vector2>(this.gamepad.leftStick, Vector2.up);
-            yield return ValidateMovement(time, Vector3.forward * time * character.playerSpeed);
+            this.Set(this.gamepad.leftStick, Vector2.up);
+            yield return this.ValidateMovement(time, Vector3.forward * time * this.character.playerSpeed);
         }
 
         /// <summary>
         /// Stop when the player stops pressing forward
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Enumerator test events.</returns>
         [UnityTest]
         public IEnumerator CharacterControlsMoveAndStop()
         {
             float time = 1.0f;
 
             // Set input action to move forward
-            Set<Vector2>(this.gamepad.leftStick, Vector2.up);
-            yield return ValidateMovement(time, Vector3.forward * time * character.playerSpeed);
+            this.Set(this.gamepad.leftStick, Vector2.up);
+            yield return this.ValidateMovement(time, Vector3.forward * time * this.character.playerSpeed);
 
             // Set input action to not move
-            Set<Vector2>(this.gamepad.leftStick, Vector2.zero);
-            yield return ValidateMovement(time, Vector3.zero);
+            this.Set(this.gamepad.leftStick, Vector2.zero);
+            yield return this.ValidateMovement(time, Vector3.zero);
         }
 
         /// <summary>
         /// Rotate based on the mouse movement
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Enumerator test events.</returns>
         [UnityTest]
         public IEnumerator CharacterControlsRotate()
         {
             // Measure starting attitude
-            Quaternion start = character.transform.rotation;
+            Quaternion start = this.character.transform.rotation;
 
             // Set input action to look to the left
-            Set<Vector2>(this.gamepad.rightStick, Vector2.left);
+            this.Set(this.gamepad.rightStick, Vector2.left);
 
             // Wait for a second
             float time = 1.0f;
             yield return new WaitForSeconds(time);
-            
+
             // Assert that the player has moved forward
-            Quaternion end = character.transform.rotation;
-            Quaternion expected = start * Quaternion.Euler(0, time * character.rotationSpeed, 0);
+            Quaternion end = this.character.transform.rotation;
+            Quaternion expected = start * Quaternion.Euler(0, time * this.character.rotationSpeed, 0);
             float delta = Quaternion.Angle(end, expected);
 
             Assert.IsTrue(
@@ -135,15 +161,15 @@ namespace CCDemo.Tests
         /// <summary>
         /// Move in the direction the player is looking
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Enumerator test events.</returns>
         [UnityTest]
         public IEnumerator CharacterControlsMoveInDirection()
         {
             // Set the player facing to the right and move forward
-            character.transform.rotation = Quaternion.LookRotation(Vector3.left, Vector3.up);
+            this.character.transform.rotation = Quaternion.LookRotation(Vector3.left, Vector3.up);
             float time = 1.0f;
-            Set<Vector2>(this.gamepad.leftStick, Vector2.up);
-            yield return ValidateMovement(time, Vector3.left * time * character.playerSpeed);
+            this.Set(this.gamepad.leftStick, Vector2.up);
+            yield return this.ValidateMovement(time, Vector3.left * time * this.character.playerSpeed);
         }
     }
 }
